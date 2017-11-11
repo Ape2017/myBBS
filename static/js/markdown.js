711bfee97fc8748390a94e540dec711a5e7fe421
DESKTOP-CF5R1ST\H-cym <cgd214@gmail.com>
2017-11-11 22:50:33 +0800

    修正 Markdown 引用显示

--- static/js/markdown.js
+++ static/js/markdown.js
@@ -8,7 +8,7 @@ var markContents = function () {
     var contents = document.querySelectorAll('.markdown-text')
     for (var i = 0; i < contents.length; i++) {
         var content = contents[i]
-        content.innerHTML = marked(content.innerHTML)
+        content.innerHTML = marked(content.innerHTML.replace(/&gt;+/g, '>'))
     }
 }
 
