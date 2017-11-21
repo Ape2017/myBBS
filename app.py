b3d49ff1dc3134c8655b1354e0272648ca03c1f8
DESKTOP-CF5R1ST\H-cym <cgd214@gmail.com>
2017-11-21 20:38:47 +0800

    config

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from config import SECRET_KEY
 from routes.index import main as index_routes
 from routes.user import main as user_routes
 from routes.admin import main as admin_routes
@@ -9,7 +10,8 @@ from routes.mail import main as mail_routes
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config')
+    # 设置 secret_key 以使用 flask 自带的 session
+    app.secret_key = SECRET_KEY
     app.register_blueprint(index_routes)
     app.register_blueprint(user_routes, url_prefix='/user')
     app.register_blueprint(admin_routes, url_prefix='/admin')
